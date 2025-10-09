1893df4c20bf166b66e36703c443f6b87a256970
anvnh <anvo20052@gmail.com>
2025-10-09 22:34:42 +0700

    commit

--- flake.nix
+++ flake.nix
@@ -1,25 +1,26 @@
 {
   description = "LinuxUtils runnable on NixOS without system deps";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
   outputs = { self, nixpkgs }:
   let
-    forAll = f: nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ]
-      (system: f (import nixpkgs { inherit system; }));
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    forAll = f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs { inherit system; }));
     deps = pkgs: with pkgs; [
       bash coreutils gnugrep gnused gawk findutils
       curl wget git jq yq-go
       desktop-file-utils shared-mime-info imagemagick libarchive
       fzf gtk3 kdePackages.kservice
       xorg.xprop xorg.xrandr
-      fuse appimage-run # hỗ trợ AppImage
+      fuse appimage-run
     ];
-  in {
+  in
+  {
     packages = forAll (pkgs: {
       lutils = pkgs.writeShellApplication {
         name = "lutils";
         runtimeInputs = deps pkgs;
-        # Script đã dùng ${BASH_SOURCE[0]} nên tìm đúng subdir khi đóng gói.
         text = builtins.readFile ./lutils.sh;
       };
     });
@@ -39,4 +40,5 @@
       default = pkgs.mkShell { buildInputs = deps pkgs; };
     });
   };
-};
+}
+
